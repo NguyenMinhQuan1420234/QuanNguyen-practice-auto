c61118d12609a31ec6860eb9d03144262c3661a4
Nguyễn Minh Quân <35888563+NguyenMinhQuan1420234@users.noreply.github.com>
2022-07-10 21:44:19 +0700

    update

--- selenium-testng/src/test/java/com/quannguyen/tests/LoginTest.java
+++ selenium-testng/src/test/java/com/quannguyen/tests/LoginTest.java
@@ -7,8 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -19,16 +23,16 @@ import com.quannguyen.pages.BasePage;
 import com.quannguyen.pages.HomePage;
 import com.quannguyen.pages.LoginPage;
 import com.quannguyen.pages.NavigationPage;
-
-import java.time.Duration;
-
-import java.lang.reflect.Method;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static com.quannguyen.utils.extentreports.ExtentTestManager.startTest;
 
-
+import java.lang.reflect.Method;
 
 @Listeners({ ExtentIReporterSuiteListenerAdapter.class })
 public class LoginTest extends BaseTest {
@@ -36,33 +40,47 @@ public class LoginTest extends BaseTest {
     public WebDriver driver;
     LoginPage loginPage;
     NavigationPage navigatePage;
-    private By BASEPAGE_APPEAR = By.xpath("//div[@id='app']");
+    static ExtentReports report;
+    static ExtentTest test;
 
     @BeforeMethod
     public void BeforeMethod() {
- 
+
         // System.setProperty("webdriver.chrome.driver", "C:/Users/banch/TestNG/demo/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        report = new ExtentReports("C:/Users/banch/QuanNguyen-practice-auto/selenium-testng/extent-reports/report.html", true);
+        test = report.startTest("Extent report demo");
         driver.get(ConfigConstants.BASE_URL);
 
     }
 
+    @AfterMethod
+    public void AfterMethod() {
+        report.endTest(test);
+        report.flush();
+        driver.close();
+    }
+
     @Test(priority = 0, description = "Login successfully.")
     public void openBrowser(Method method) {
+        test.log(LogStatus.INFO, "testing Browser working normally starts.");
+        startTest(method.getName(), "Open browser.");
         System.out.println("Checking if browser working right.");
+
+        test.log(LogStatus.PASS, "Browser is working as intend.");
+
     }
 
     @Test(priority = 1, description = "Login successfully.")
     public void LoginSuccessfulCase(Method method) throws InterruptedException {
+        test.log(LogStatus.INFO, "Test case: Login successfully starts.");
 
         startTest(method.getName(), "Login with valid username and password.");
 
         loginPage = new LoginPage(driver);
         navigatePage = new NavigationPage(driver);
-        
-        navigatePage.waitForElmentToBeVisibile(BASEPAGE_APPEAR);
 
         try {
             loginPage.navigate(UrlConstants.LOGIN_URL);
@@ -76,10 +94,13 @@ public class LoginTest extends BaseTest {
         loginPage.ClickLoginBtn();
 
         assertThat("verify message", navigatePage.getUsername(), equalTo(ConfigConstants.USERNAME));
+        test.log(LogStatus.PASS, "Log in successfully");
+
     }
 
     @Test(priority = 1, description = "Login Unsuccessfully with empty username and password.")
     public void LoginUnsuccessfully(Method method) {
+        test.log(LogStatus.INFO, "Test case: invalid Login starts.");
 
         startTest(method.getName(), "Login with invalid username and password.");
 
@@ -92,10 +113,8 @@ public class LoginTest extends BaseTest {
         loginPage.ClickLoginBtn();
 
         assertThat("verify message",  navigatePage.getClassAttribute(), equalTo(ConfigConstants.USERNAME_CLASS_INVALID));
+        test.log(LogStatus.PASS, "Log in fail");
+
     }
 
-    @AfterMethod
-    public void AfterMethod() {
-        driver.quit();
-    }
 }
